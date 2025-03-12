ade0ee6ecff079b41e05832a82ccc24a3bb3134b
tanis <tanisth.hota@gmail.com>
2025-03-12 21:47:20 +0530

    made changes to cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "hi i am Tanistha Hota \n";
+
   return 0;
 }
